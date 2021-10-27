3bbf68ab8fd2278da305dc67f1506c3a413bfd80
Muhammet Kömür <34505894+muhammet075@users.noreply.github.com>
2021-10-27 10:45:00 +0200

    huisdier namen opschonen

---
+++ Huisdier namen/javascript.js
@@ -0,0 +1,14 @@
+//nieuwe lege array
+let huisdierData = [];
+
+//loop voor de data en de favoriete huisdier in de nieuwe array pushen
+for (let i = 0; i < data.length; i++) {
+  huisdierData.push("<li>" + data[i]["favoriet-huisdier"] + "</li>");
+}
+
+//nieuwe variabel voor de lijst
+var tekst = document.querySelector(".dataLijst");
+
+//de nieuwe array met de huisideren wordt in de html lijst omgezet en krijgt een class hoofdletter en met css krijgt de eerste letter een hoofdletter
+tekst.innerHTML = huisdierData;
+tekst.classList.add("hoofdletter");
